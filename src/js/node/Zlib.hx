293e82de23a6141834bd34a22f97b0f4ae7f87d8
akitsu-sanae <akitsu.sanae@gmail.com>
2019-09-12 19:05:10 +0900

    update zlib extern

--- src/js/node/Zlib.hx
+++ src/js/node/Zlib.hx
@@ -23,6 +23,7 @@
 package js.node;
 
 import haxe.extern.EitherType;
+import haxe.Constraints; // for haxe.Function
 #if haxe4
 import js.lib.Error;
 #else
@@ -36,6 +37,11 @@ typedef ZlibOptions = {
 		default: `Zlib.Z_NO_FLUSH`
 	**/
 	@:optional var flush:Int;
+
+	/**
+		default: `Zlib.Z_FINISH`
+	**/
+	@:optional var finishFlush:Int;
 
 	/**
 		default: 16*1024
@@ -63,11 +69,76 @@ typedef ZlibOptions = {
 		deflate/inflate only, empty dictionary by default
 	**/
 	@:optional var dictionary:Buffer;
+
+	/**
+		If `true`, returns an object with `buffer` and `engine`
+	**/
+	@:optional var info:Bool;
+}
+
+enum BrotliParamMode {
+	@:native("BROTLI_MODE_GENERIC")
+	BROTLI_MODE_GENERIC;
+	@:native("BROTLI_MODE_TEXT")
+	BROTLI_MODE_TEXT;
+	@:native("BROTLI_MODE_FONT")
+	BROTLI_MODE_FONT;
 }
 
 /**
-	This provides bindings to Gzip/Gunzip, Deflate/Inflate, and DeflateRaw/InflateRaw classes.
-	Each class takes the same options, and is a readable/writable Stream.
+	There are several options that can be set on Brotli encoders, affecting compression efficiency and speed.
+
+	@see https://nodejs.org/dist/latest-v12.x/docs/api/zlib.html#zlib_compressor_options
+**/
+typedef BrotliParams = {
+	@:optional var BROTLI_PARAM_MODE:BrotliParamMode;
+	@:optional var BROTLI_PARAM_QUALITY:Int;
+	@:optional var BROTLI_PARAM_SIZE_HINT:Int;
+	@:optional var BROTLI_PARAM_LGWIN:Int;
+	@:optional var BROTLI_PARAM_LGBLOCK:Int;
+	@:optional var BROTLI_PARAM_DISABLE_LITERAL_CONTEXT_MODELING:Bool;
+	@:optional var BROTLI_PARAM_LARGE_WINDOW:Bool;
+	@:optional var BROTLI_PARAM_NPOSTFIX:Int;
+	@:optional var BROTLI_PARAM_NDIRECT:Int;
+}
+
+/**
+	Advanced options for controlling Brotli Decompression:
+**/
+typedef BrotliDecompressOptions = {
+	@:optional var BROTLI_DECODER_PARAM_DISABLE_RING_BUFFER_REALLOCATION:Bool;
+	@:optional var BROTLI_DECODER_PARAM_LARGE_WINDOW:Bool;
+}
+
+/**
+	Each Brotli-based class takes an options object. All options are optional.
+
+	@see https://nodejs.org/dist/latest-v12.x/docs/api/zlib.html#zlib_class_brotlioptions
+ **/
+typedef BrotliOptions = {
+
+	/**
+		default: `Zlib.BROTLI_OPERATION_PROCESS`
+	**/
+	@:optional var flush:Int;
+
+	/**
+		default: `Zlib.BROTLI_OPERATION_FINISH`
+	**/
+	@:optional var finishFlush:Int;
+
+	/**
+		default: `16*1024`
+	**/
+	@:optional var chunkSize:Int;
+
+	@:optional var params: BrotliParams;
+}
+
+/**
+	The zlib module provides compression functionality implemented using Gzip and Deflate/Inflate, as well as Brotli.
+
+	@see https://nodejs.org/dist/latest-v12.x/docs/api/zlib.html#zlib_zlib
 **/
 @:jsRequire("zlib")
 extern class Zlib {
@@ -118,133 +189,171 @@ extern class Zlib {
 	static var Z_DEFAULT_STRATEGY(default, null):Int;
 
 	/**
-		Possible values of the data_type field.
-	**/
-	static var Z_BINARY(default, null):Int;
+		Brotli Flush operation constants
 
-	static var Z_TEXT(default, null):Int;
-	static var Z_ASCII(default, null):Int;
-	static var Z_UNKNOWN(default, null):Int;
+		@see https://nodejs.org/dist/latest-v12.x/docs/api/zlib.html#zlib_flush_operations
+	**/
+	static var BROTLI_OPERATION_PROCESS(default, null):Int;
+	static var BROTLI_OPERATION_FLUSH(default, null):Int;
+	static var BROTLI_OPERATION_FINISH(default, null):Int;
+	static var BROTLI_OPERATION_EMIT_METADATA(default, null):Int;
 
 	/**
-		The deflate compression method (the only one supported in this version).
+		Deprecated alias for `bytesWritten`.
 	**/
-	static var Z_DEFLATED(default, null):Int;
+	@:deprecated
+	static var bytesRead(default, null):Int;
 
 	/**
-		For initializing zalloc, zfree, opaque.
+		the number of bytes written to the engine, before the bytes are processed (compressed or decompressed, as appropriate for the derived class).
 	**/
-	static var Z_NULL(default, null):Int;
+	static var bytesWritten(default, null):Int;
 
 	/**
-		Returns a new `Gzip` object with an `options`.
+		Close the underlying handle.
 	**/
-	static function createGzip(?options:ZlibOptions):Gzip;
+	static function close(callback: haxe.Function):Void;
 
 	/**
-		Returns a new `Gunzip` object with an `options`.
+		Flush pending data. Don't call this frivolously, premature flushes negatively impact the effectiveness of the compression algorithm.
+
+		@see https://nodejs.org/dist/latest-v12.x/docs/api/zlib.html#zlib_zlib_flush_kind_callback
 	**/
-	static function createGunzip(?options:ZlibOptions):Gunzip;
+	static function flush(?kind:Int,callback:haxe.Function):Void;
 
 	/**
-		Returns a new `Deflate` object with an `options`.
+		This function is only available for zlib-based streams, i.e. not Brotli.
+
+		Dynamically update the compression level and compression strategy. Only applicable to deflate algorithm.
+	**/
+	static function params(level:Int, strategy:Int, callback:haxe.Function):Void;
+
+	/**
+		Reset the compressor/decompressor to factory defaults. Only applicable to the inflate and deflate algorithms.
+	**/
+	static function reset():Void;
+
+	/**
+		Creates and returns a new `BrotliCompress` object.
+	**/
+	static function createBrotliCompress(?options: BrotliOptions):BrotliCompress;
+
+	/**
+		Creates and returns a new `BrotliDecompress` object.
+	**/
+	static function createBrotliDecompress(?options: BrotliOptions):BrotliDecompress;
+
+	/**
+		Creates and returns a new `Deflate` object.
 	**/
 	static function createDeflate(?options:ZlibOptions):Deflate;
 
 	/**
-		Returns a new `Inflate` object with an `options`.
-	**/
-	static function createInflate(?options:ZlibOptions):Inflate;
-
-	/**
-		Returns a new `DeflateRaw` object with an `options`.
+		Creates and returns a new `DeflateRaw` object.
+		@see https://nodejs.org/dist/latest-v12.x/docs/api/zlib.html#zlib_zlib_createdeflateraw_options
 	**/
 	static function createDeflateRaw(?options:ZlibOptions):DeflateRaw;
 
 	/**
-		Returns a new `InflateRaw` object with an `options`.
+		Creates and returns a new `Gunzip` object.
+	**/
+	static function createGunzip(?options:ZlibOptions):Gunzip;
+
+	/**
+		Creates and returns a new `Gzip` object.
+	**/
+	static function createGzip(?options:ZlibOptions):Gzip;
+
+	/**
+		Creates and returns a new `Inflate` object.
+	**/
+	static function createInflate(?options:ZlibOptions):Inflate;
+	/**
+		Creates and returns a new `InflateRaw` object.
 	**/
 	static function createInflateRaw(?options:ZlibOptions):InflateRaw;
 
 	/**
-		Returns a new `Unzip` object with an `options`.
+		Creates and returns a new `Unzip` object.
 	**/
 	static function createUnzip(?options:ZlibOptions):Unzip;
 
 	/**
-		Compress a string with `Deflate`.
+		Compress a string with `Brotli`.
 	**/
-	@:overload(function(buf:EitherType<String, Buffer>, options:ZlibOptions, callback:Error->Buffer->Void):Void {})
-	static function deflate(buf:EitherType<String, Buffer>, callback:Error->Buffer->Void):Void;
+	@:overload(function(buf:JsBuffer, options:BrotliOptions, callback:Error->Buffer->Void):Void {})
+	static function brotliCompress(buf:JsBuffer, callback:Error->Buffer->Void):Void;
 
 	/**
-		Compress a string with `Deflate` (synchronous version).
+		Compress a chunk of data with `BrotliCompress`.
 	**/
-	static function deflateSync(buf:EitherType<String, Buffer>, ?options:ZlibOptions):Buffer;
+	@:overload(function(buf:JsBuffer, options:BrotliOptions):Void {})
+	static function brotliCompressSync(buf:JsBuffer, options: BrotliOptions):Void;
+
+	@:overload(function(buf:JsBuffer, options:BrotliOptions):Void {})
+	static function brotliDecompress(buf:JsBuffer, options: BrotliOptions):Void;
 
 	/**
-		Compress a string with `DeflateRaw`.
+		Decompress a chunk of data with `BrotliDecompress`.
 	**/
-	@:overload(function(buf:EitherType<String, Buffer>, options:ZlibOptions, callback:Error->Buffer->Void):Void {})
-	static function deflateRaw(buf:EitherType<String, Buffer>, callback:Error->Buffer->Void):Void;
+	static function brotliDecompressSync(buf:JsBuffer,options:BrotliOptions):Void;
+
+	@:overload(function(buf:JsBuffer, options:ZlibOptions, callback:Error->Buffer->Void):Void {})
+	static function deflate(buf:JsBuffer, callback:Error->Buffer->Void):Void;
 
 	/**
-		Compress a string with `DeflateRaw` (synchronous version).
+		Compress a chunk of data with `Deflate`.
 	**/
-	static function deflateRawSync(buf:EitherType<String, Buffer>, ?options:ZlibOptions):Buffer;
+	static function deflateSync(buf:JsBuffer, ?options:ZlibOptions):Buffer;
+
+	@:overload(function(buf:JsBuffer, options:ZlibOptions, callback:Error->Buffer->Void):Void {})
+	static function deflateRaw(buf:JsBuffer, callback:Error->Buffer->Void):Void;
 
 	/**
-		Compress a string with `Gzip`.
+		Compress a chunk of data with `DeflateRaw`.
 	**/
-	@:overload(function(buf:EitherType<String, Buffer>, options:ZlibOptions, callback:Error->Buffer->Void):Void {})
-	static function gzip(buf:EitherType<String, Buffer>, callback:Error->Buffer->Void):Void;
+	static function deflateRawSync(buf:JsBuffer, ?options:ZlibOptions):Buffer;
+
+	@:overload(function(buf:JsBuffer, options:ZlibOptions, callback:Error->Buffer->Void):Void {})
+	static function gunzip(buf:JsBuffer, callback:Error->Buffer->Void):Void;
+
 
 	/**
-		Compress a string with `Gzip` (synchronous version).
+		Decompress a chunk of data with `Gunzip`.
 	**/
-	static function gzipSync(buf:EitherType<String, Buffer>, ?options:ZlibOptions):Buffer;
+	static function gunzipSync(buf:JsBuffer, ?options:ZlibOptions):Buffer;
+
+	@:overload(function(buf:JsBuffer, options:ZlibOptions, callback:Error->Buffer->Void):Void {})
+	static function gzip(buf:JsBuffer, callback:Error->Buffer->Void):Void;
 
 	/**
-		Decompress a raw Buffer with `Gunzip`.
+		Compress a chunk of data with `Gzip`.
 	**/
-	@:overload(function(buf:EitherType<String, Buffer>, options:ZlibOptions, callback:Error->Buffer->Void):Void {})
-	static function gunzip(buf:EitherType<String, Buffer>, callback:Error->Buffer->Void):Void;
+	static function gzipSync(buf:JsBuffer, ?options:ZlibOptions):Buffer;
+
+	@:overload(function(buf:JsBuffer, options:ZlibOptions, callback:Error->Buffer->Void):Void {})
+	static function inflate(buf:JsBuffer, callback:Error->Buffer->Void):Void;
 
 	/**
-		Decompress a raw Buffer with `Gunzip` (synchronous version).
+		Decompress a chunk of data with `Inflate`.
 	**/
-	static function gunzipSync(buf:EitherType<String, Buffer>, ?options:ZlibOptions):Buffer;
+	static function inflateSync(buf:JsBuffer, ?options:ZlibOptions):Buffer;
+
+	@:overload(function(buf:JsBuffer, options:ZlibOptions, callback:Error->Buffer->Void):Void {})
+	static function inflateRaw(buf:JsBuffer, callback:Error->Buffer->Void):Void;
 
 	/**
-		Decompress a raw Buffer with `Inflate`.
+		Decompress a chunk of data with `InflateRaw`.
 	**/
-	@:overload(function(buf:EitherType<String, Buffer>, options:ZlibOptions, callback:Error->Buffer->Void):Void {})
-	static function inflate(buf:EitherType<String, Buffer>, callback:Error->Buffer->Void):Void;
+	static function inflateRawSync(buf:JsBuffer, ?options:ZlibOptions):Buffer;
+
+	@:overload(function(buf:JsBuffer, options:ZlibOptions, callback:Error->Buffer->Void):Void {})
+	static function unzip(buf:JsBuffer, callback:Error->Buffer->Void):Void;
 
 	/**
-		Decompress a raw Buffer with `Inflate` (synchronous version).
+		Decompress a chunk of data with `Unzip`.
 	**/
-	static function inflateSync(buf:EitherType<String, Buffer>, ?options:ZlibOptions):Buffer;
-
-	/**
-		Decompress a raw Buffer with `InflateRaw`.
-	**/
-	@:overload(function(buf:EitherType<String, Buffer>, options:ZlibOptions, callback:Error->Buffer->Void):Void {})
-	static function inflateRaw(buf:EitherType<String, Buffer>, callback:Error->Buffer->Void):Void;
-
-	/**
-		Decompress a raw Buffer with `InflateRaw` (synchronous version).
-	**/
-	static function inflateRawSync(buf:EitherType<String, Buffer>, ?options:ZlibOptions):Buffer;
-
-	/**
-		Decompress a raw Buffer with `Unzip`.
-	**/
-	@:overload(function(buf:EitherType<String, Buffer>, options:ZlibOptions, callback:Error->Buffer->Void):Void {})
-	static function unzip(buf:EitherType<String, Buffer>, callback:Error->Buffer->Void):Void;
-
-	/**
-		Decompress a raw Buffer with `Unzip` (synchronous version).
-	**/
-	static function unzipSync(buf:EitherType<String, Buffer>, ?options:ZlibOptions):Buffer;
+	static function unzipSync(buf:JsBuffer, ?options:ZlibOptions):Buffer;
 }
+
+private typedef JsBuffer = EitherType<String, Buffer>;
