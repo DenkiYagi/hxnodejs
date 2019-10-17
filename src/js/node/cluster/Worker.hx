1da00f991c92e5645bf328523d83b3274265f07a
ahuglajbclajep <ahuglajbclajep@gmail.com>
2019-10-17 17:55:43 +0900

    little fix

--- src/js/node/cluster/Worker.hx
+++ src/js/node/cluster/Worker.hx
@@ -81,6 +81,8 @@ import js.Error;
 
 /**
 	A `Worker` object contains all public information and method about a worker.
+	In the master it can be obtained using `cluster.workers`.
+	In a worker it can be obtained using `cluster.worker`.
 
 	@see https://nodejs.org/api/cluster.html#cluster_class_worker
 **/
