de56a27014feb70ccd426ef33c1327a3f7cf03bf
akitsu-sanae <akitsu.sanae@gmail.com>
2020-01-29 17:03:36 +0900

    add SystemError Constants

--- src/js/node/error/SystemError.hx
+++ src/js/node/error/SystemError.hx
@@ -88,3 +88,152 @@ extern class SystemError extends Error {
 	**/
 	@:optional public var syscall(default, never):String;
 }
+
+/**
+	This is a list of system errors commonly-encountered when writing a Node.js program.
+	For a comprehensive list, see the errno(3) man page.
+
+	@see https://nodejs.org/api/errors.html#errors_common_system_errors
+**/
+class CommonSystemErrors {
+	/**
+		`EACCES` (Permission denied):
+		An attempt was made to access a file in a way forbidden by its file access permissions.
+	**/
+	public static var EACCES(get, never):Int;
+
+	private static inline function get_EACCES():Int
+		return js.Syntax.code("EACCES");
+
+	/**
+		`EADDRINUSE` (Address already in use):
+		An attempt to bind a server (`net`, `http`, or `https`) to a local address failed
+		due to another server on the local system already occupying that address.
+	**/
+	public static var EADDRINUSE(get, never):Int;
+
+	private static inline function get_EADDRINUSE():Int
+		return js.Syntax.code("EADDRINUSE");
+
+	/**
+		`ECONNREFUSED` (Connection refused):
+		No connection could be made because the target machine actively refused it.
+		This usually results from trying to connect to a service that is inactive on the foreign host.
+	**/
+	public static var ECONNREFUSED(get, never):Int;
+
+	private static inline function get_ECONNREFUSED():Int
+		return js.Syntax.code("ECONNREFUSED");
+
+	/**
+		`ECONNRESET` (Connection reset by peer):
+		A connection was forcibly closed by a peer.
+		This normally results from a loss of the connection on the remote socket due to a timeout or reboot.
+		Commonly encountered via the http and net modules.
+	**/
+	public static var ECONNRESET(get, never):Int;
+
+	private static inline function get_ECONNRESET():Int
+		return js.Syntax.code("ECONNRESET");
+
+	/**
+		`EEXIST` (File exists):
+		An existing file was the target of an operation that required that the target not exist.
+	**/
+	public static var EEXIST(get, never):Int;
+
+	private static inline function get_EEXIST():Int
+		return js.Syntax.code("EEXIST");
+
+	/**
+		`EISDIR` (Is a directory):
+		An operation expected a file, but the given pathname was a directory.
+	**/
+	public static var EISDIR(get, never):Int;
+
+	private static inline function get_EISDIR():Int
+		return js.Syntax.code("EISDIR");
+
+	/**
+		`EMFILE` (Too many open files in system):
+		Maximum number of file descriptors allowable on the system has been reached,
+		and requests for another descriptor cannot be fulfilled until at least one has been closed.
+		This is encountered when opening many files at once in parallel,
+		especially on systems (in particular, macOS) where there is a low file descriptor limit for processes.
+		To remedy a low limit, run `ulimit -n 2048` in the same shell that will run the Node.js process.
+	**/
+	public static var EMFILE(get, never):Int;
+
+	private static inline function get_EMFILE():Int
+		return js.Syntax.code("EMFILE");
+
+	/**
+		`ENOENT` (No such file or directory):
+		Commonly raised by `fs` operations to indicate that a component of the specified
+		pathname does not exist — no entity (file or directory) could be found by the given path.
+	**/
+	public static var ENOENT(get, never):Int;
+
+	private static inline function get_ENOENT():Int
+		return js.Syntax.code("ENOENT");
+
+	/**
+		`ENOTDIR` (Not a directory):
+		A component of the given pathname existed, but was not a directory as expected.
+		Commonly raised by `fs.readdir`.
+	**/
+	public static var ENOTDIR(get, never):Int;
+
+	private static inline function get_ENOTDIR():Int
+		return js.Syntax.code("ENOTDIR");
+
+	/**
+		`ENOTEMPTY` (Directory not empty):
+		A directory with entries was the target of an operation that requires an empty directory
+		— usually `fs.unlink`.
+	**/
+	public static var ENOTEMPTY(get, never):Int;
+
+	private static inline function get_ENOTEMPTY():Int
+		return js.Syntax.code("ENOTEMPTY");
+
+	/**
+		`ENOTFOUND` (DNS lookup failed):
+		Indicates a DNS failure of either `EAI_NODATA` or `EAI_NONAME`.
+		This is not a standard POSIX error.
+	**/
+	public static var ENOTFOUND(get, never):Int;
+
+	private static inline function get_ENOTFOUND():Int
+		return js.Syntax.code("ENOTFOUND");
+
+	/**
+		`EPERM` (Operation not permitted):
+		An attempt was made to perform an operation that requires elevated privileges.
+	**/
+	public static var EPERM(get, never):Int;
+
+	private static inline function get_EPERM():Int
+		return js.Syntax.code("EPERM");
+
+	/**
+		`EPIPE` (Broken pipe):
+		A write on a pipe, socket, or FIFO for which there is no process to read the data.
+		Commonly encountered at the net and http layers, indicative that the remote side of the stream being written to has been closed.
+	**/
+	public static var EPIPE(get, never):Int;
+
+	private static inline function get_EPIPE():Int
+		return js.Syntax.code("EPIPE");
+
+	/**
+		`ETIMEDOUT` (Operation timed out):
+		A connect or send request failed because
+		the connected party did not properly respond after a period of time.
+		Usually encountered by http or net — often a sign that a `socket.end()` was not properly called.
+	**/
+	public static var ETIMEOUT(get, never):Int;
+
+	private static inline function get_ETIMEOUT():Int
+		return js.Syntax.code("ETIMEOUT");
+}
